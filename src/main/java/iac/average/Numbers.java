33cf63af5750ce19e71e2c8b7a8f03e3f30a03a3
Chris <c_de_vos8@hotmail.com>
2017-03-12 16:22:10 +0100

    First error test

--- src/main/java/iac/average/Numbers.java
+++ src/main/java/iac/average/Numbers.java
@@ -1,12 +1,9 @@
 
 package iac.average;
 
-import java.math.BigInteger;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlSchemaType;
 import javax.xml.bind.annotation.XmlType;
 
 
@@ -20,10 +17,10 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
- *         &lt;element name="number1" type="{http://www.w3.org/2001/XMLSchema}positiveInteger"/>
- *         &lt;element name="number2" type="{http://www.w3.org/2001/XMLSchema}positiveInteger"/>
- *         &lt;element name="number3" type="{http://www.w3.org/2001/XMLSchema}positiveInteger"/>
- *         &lt;element name="number4" type="{http://www.w3.org/2001/XMLSchema}positiveInteger"/>
+ *         &lt;element name="number1" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="number2" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="number3" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="number4" type="{http://www.w3.org/2001/XMLSchema}int"/>
  *       &lt;/sequence>
  *     &lt;/restriction>
  *   &lt;/complexContent>
@@ -42,112 +39,72 @@ import javax.xml.bind.annotation.XmlType;
 @XmlRootElement(name = "numbers")
 public class Numbers {
 
-    @XmlElement(required = true)
-    @XmlSchemaType(name = "positiveInteger")
-    protected BigInteger number1;
-    @XmlElement(required = true)
-    @XmlSchemaType(name = "positiveInteger")
-    protected BigInteger number2;
-    @XmlElement(required = true)
-    @XmlSchemaType(name = "positiveInteger")
-    protected BigInteger number3;
-    @XmlElement(required = true)
-    @XmlSchemaType(name = "positiveInteger")
-    protected BigInteger number4;
+    protected int number1;
+    protected int number2;
+    protected int number3;
+    protected int number4;
 
     /**
      * Gets the value of the number1 property.
      * 
-     * @return
-     *     possible object is
-     *     {@link BigInteger }
-     *     
      */
-    public BigInteger getNumber1() {
+    public int getNumber1() {
         return number1;
     }
 
     /**
      * Sets the value of the number1 property.
      * 
-     * @param value
-     *     allowed object is
-     *     {@link BigInteger }
-     *     
      */
-    public void setNumber1(BigInteger value) {
+    public void setNumber1(int value) {
         this.number1 = value;
     }
 
     /**
      * Gets the value of the number2 property.
      * 
-     * @return
-     *     possible object is
-     *     {@link BigInteger }
-     *     
      */
-    public BigInteger getNumber2() {
+    public int getNumber2() {
         return number2;
     }
 
     /**
      * Sets the value of the number2 property.
      * 
-     * @param value
-     *     allowed object is
-     *     {@link BigInteger }
-     *     
      */
-    public void setNumber2(BigInteger value) {
+    public void setNumber2(int value) {
         this.number2 = value;
     }
 
     /**
      * Gets the value of the number3 property.
      * 
-     * @return
-     *     possible object is
-     *     {@link BigInteger }
-     *     
      */
-    public BigInteger getNumber3() {
+    public int getNumber3() {
         return number3;
     }
 
     /**
      * Sets the value of the number3 property.
      * 
-     * @param value
-     *     allowed object is
-     *     {@link BigInteger }
-     *     
      */
-    public void setNumber3(BigInteger value) {
+    public void setNumber3(int value) {
         this.number3 = value;
     }
 
     /**
      * Gets the value of the number4 property.
      * 
-     * @return
-     *     possible object is
-     *     {@link BigInteger }
-     *     
      */
-    public BigInteger getNumber4() {
+    public int getNumber4() {
         return number4;
     }
 
     /**
      * Sets the value of the number4 property.
      * 
-     * @param value
-     *     allowed object is
-     *     {@link BigInteger }
-     *     
      */
-    public void setNumber4(BigInteger value) {
+    public void setNumber4(int value) {
         this.number4 = value;
     }
 
